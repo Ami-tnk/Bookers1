40a235a0fc96257e5ccfcda639d555fdc04bdae6
Ami-tnk <ami1y251992@gmail.com>
2020-11-19 03:35:06 +0000

    [Add]top画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # homesコントローラのtopアクション(トップ画面)をルートパスに設定
+  root to: 'homes#top'
+
+  get '/' => 'homes#top'
 end
