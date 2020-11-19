8e88c1d272d7a91d45984a63a95d2d25c848143c
Ami-tnk <ami1y251992@gmail.com>
2020-11-19 06:47:31 +0000

    [Add]詳細画面,編集画面作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,22 @@
 class BooksController < ApplicationController
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
-  end
-
   def index
     @book = Book.new
     @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
 
   private
