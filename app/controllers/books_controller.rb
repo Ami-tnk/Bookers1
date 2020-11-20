1098902f02a3886d044639a439b98022b51254a4
Ami-tnk <ami1y251992@gmail.com>
2020-11-20 09:29:07 +0000

    [Add]orderメソッド, validatesによる空欄チェック

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,15 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    @books = Book.order("id")#デフォルトでASCに指定されておりIDが若い順に表示
   end
 
   def create
     book = Book.new(book_params)
     if book.save
       redirect_to book_path(book.id), notice:"Book was successfully created."
+    else
+      redirect_to books_path, alert:"Error! Please fill in all."
     end
   end
 
@@ -22,8 +24,11 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path, notice:"Book was successfully updated."
+    if book.update(book_params)
+      redirect_to book_path, notice:"Book was successfully updated."
+    else
+      redirect_to edit_book_path(book.id), alert:"Error! Please fill in all."
+    end
   end
 
   def destroy
