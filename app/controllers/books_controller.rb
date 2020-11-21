3b372503609998a5cd5b329edd3f64c28ad34f28
Ami-tnk <ami1y251992@gmail.com>
2020-11-21 15:11:06 +0000

    [Finish]Add バリデーションエラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id), notice:"Book was successfully created."
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book.id), notice:"Book was successfully created." #データ更新/削除が必要な場合(controllerの処理が必要)に使用
     else
       @books = Book.order("id")
-      render template: "books/index"
+      render :index  #ログインや入力形式に失敗した場合など(ただエラーを表示させるだけ)に使用
     end
   end
 
   def index
-    @book = Book.new
-    @books = Book.order("id")#デフォルトでASCに指定されておりIDが若い順に表示
+    @book = Book.new           #同ページに新規投稿フォームを置くためrender使用しているのでメソッドが必要
+    @books = Book.order("id")  #デフォルトでASCに指定済みでIDが若い順に表示
   end
 
   def show
@@ -28,12 +29,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id), notice:"Book was successfully updated."
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice:"Book was successfully updated."
     else
-      @book = Book.find(params[:id])
-      render template: "books/edit"
+      render 'edit'
     end
   end
 
